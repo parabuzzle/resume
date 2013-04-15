c9ba1fc8999d55c0d10308df4fd81c26f102a6df
Mike Heijmans <parabuzzle@gmail.com>
2013-04-14 20:33:13 -0700

    Added majestic mustang

--- heijmans.rb
+++ heijmans.rb
@@ -4,6 +4,7 @@
 
 require 'net/http'
 require 'uri'
+require 'base64'
 
 # load my objective
 @objective = "To combine my knowledge and experience of high availability 
@@ -110,6 +111,21 @@ def listall
   end
 end
 
+def list_commands
+  puts "\n\nAvailable Commands:"
+  puts ""
+  @my_skills.keys.each do |sublist|
+    puts " - #{sublist}"
+  end
+  puts " - profiles"
+  puts " - open_source_projects"
+  puts " - download_resume"
+  puts " - listall"
+  puts " - help"
+  puts " - print majestic mustang"
+  puts " - quit"
+end
+
 def main
   puts "Michael L. Heijmans"
   puts "421 Manchester Avenue"
@@ -122,15 +138,7 @@ def main
   puts "###~~~OBJECTIVE~~~###"
   puts @objective
   puts ""
-  puts "\n\nAvailable Commands:"
-  puts ""
-  @my_skills.keys.each do |sublist|
-    puts " - #{sublist}"
-  end
-  puts " - profiles"
-  puts " - open_source_projects"
-  puts " - download_resume"
-  puts " - listall"
+  list_commands
   
 end
 
@@ -167,6 +175,12 @@ def parse_line(line)
     when /^listall/i
       listall
       return true
+    when /^print majestic mustang/i
+      print_art
+      return true
+    when /^help/i
+      list_commands
+      return true
     when /^exit|^quit/i
       raise Interrupt
     when /^(.+)/i
@@ -175,7 +189,10 @@ def parse_line(line)
   end # end case line
   return false
 end
-      
+
+def print_art
+  print Base64.decode64("CiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg\nICAgICAgYFQiLC5gLSwgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAg\nICAgICAgICAgICAgICAgICAgICAgICAgJzgsIDouIAogICAgICAgICAgICAg\nICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgYCIiYG9vb29iLiJU\nLC4gCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg\nICAgLC1gIi4pTzs4OmRvb2IuJy0uIAogICAgICAgICAgICAgICAgICAgICAg\nICAgICAgICAgICAgICAgLC4uYCcuJycgLWRQKClkOE84WW84OiwuLmAsIAog\nICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC1vOGItICAgICAs\nLi4pZG9PTzg6JzpvOyBgWTguYCwgCiAgICAgICAgICAgICAgICAgICAgICAg\nICAgICAgICAgICAsLi5iby4sLi4uLi4pT09PODg4bycgOm9PLiAgIi4gIGAt\nLiAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAsICJgImQuLi4u\nODhPT09PTzhPODhvICA6TzhvOy4gICAgOzssYiAKICAgICAgICAgICAgICAg\nICAgICAgICAgICAgICAgICxkT09PT08iIiIiIiIiIk84ODg4OG86ICA6Tzg4\nT28uOzpvODg4ZCAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICIi\nODg4T2IuLi4sLS0gOm84OE84OG86LiA6byciIiIiIiIiWThPUCAKICAgICAg\nICAgICAgICAgICAgICAgICAgICAgICAgIGQ4ODg4Li4uLi4sLi4gOm84T084\nODg6OiA6OiAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIiIgLmRP\nTzhib2AnJywsO084OE86TzhvOiA6OiwgCiAgICAgICAgICAgICAgICAgICAg\nICAgICAgICAgICAgICxkZDgiLiAgLC0pZG84TzhvOiIiIjsgOjo6IAogICAg\nICAgICAgICAgICAgICAgICAgICAgICAgICAgICAsZGIoLiAgVCk4UDo4bzo6\nOjo6ICAgIDo6OiAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg\nLSIiLGAoO08iS2RPbzo6ICAgICAgICA6OjogCiAgICAgICAgICAgICAgICAg\nICAgICAgICAgICAgICAgICAgLEssJyIuZG9vOjo6JyAgICAgICAgOm86IAog\nICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAuZG9vOjo6IiIi\nOjogIDouICAgICdvOiAKICAgICAgICAsLi4gICAgICAgICAgICAuO29vb29v\nb28uLm86IiIiIiIgICAgIDo6Oy4gOjo7LiAgJ28uIAogICAsLCAiJyAgICBg\nIC4uICAgLmQ7bzoiIiInICAgICAgICAgICAgICAgICAgOjpvOjs6Om86OiAg\nOjsgCiAgIGQsICAgICAgICAgLCAuLm9vbzo6OyAgICAgICAgICAgICAgICAg\nICAgICA6Om9vOjs6Om8iJy46byAKICAsZCcuICAgICAgIDpPT09PTzhPbzo6\nIiAnLi4gLiAgICAgICAgICAgICAgIDo6bzhPb286OyAgO286IAogICdQIiAg\nIDsgIDsuT1BkODg4OE84Ojo7LiAnb09vbzouOy4uICAgICAgICAgOzpPODhP\nb286JyBPIicgCiAgLDg6ICAgbzo6b09gIDg4ODg4T09vOjo6ICBvOE84T286\nOjo7OyAgICAgLDs6b084OE9PbzsgICcgCiAsWVAgICw6Ojs6TzogIDg4ODg4\nOG86Ojo6ICA6ODg4OE9vbzo6Ojo6Ojo6Ojpvbzg4ODg4OG87LiAsIAogJyxk\nOiA6OztPOzogICA6ODg4ODg4OjpvOyAgOjg4ODg4ODhPb29vb29vb29vbzg4\nODg4ODg4T287ICwgCiBkUFk6ICA6bzhPICAgICBZTzg4ODhPOk86OyAgTzg4\nODg4ODg4ODhPT09PODg4IiIgWThvOk84OG87ICwgCiwnIE86ICAnb2JgICAg\nICAgIjg4ODg4ODhPbzs7bzg4ODg4ODg4ODg4ODgnIicgICAgIGA4T086LmBP\nT2IgLiAKJyAgWTogICw6bzogICAgICAgYDhPODg4ODhPT29vIiIiIiIiIiIi\nIiInICAgICAgICAgICBgT09vYmBZOGJgIAogICA6OiAgJztvOiAgICAgICAg\nYDhPODhvOm9Pb1AgICAgICAgICAgICAgICAgICAgICAgICBgOE9vIGBZTy4g\nCiAgIGA6ICAgT286ICAgICAgICAgYDg4OE86Om9QICAgICAgICAgICAgICAg\nICAgICAgICAgICA4OE8gIDpPWSAKICAgIDpvOyA4b1AgICAgICAgICA6ODg4\nbzo6UCAgICAgICAgICAgICAgICAgICAgICAgICAgIGRvOiAgOE86IAogICAs\nb29POjhPJyAgICAgICAsZDg4ODhvOk8nICAgICAgICAgICAgICAgICAgICAg\nICAgICBkT28gICA7Oi4gCiAgIDtPOG9kbycgICAgICAgIDg4ODg4TzpvJyAg\nICAgICAgICAgICAgICAgICAgICAgICAgZG86OiAgb28uOiAKICBkImApOE8n\nICAgICAgICAgIllPODhPbycgICAgICAgICAgICAgICAgICAgICAgICAgICI4\nTzogICBvOGInIAogJyctJ2AiICAgICAgICAgICAgZDpPOG9LICAtaHJyLSAg\nICAgICAgICAgICAgICAgICBkT09vJyAgOm8iOiAKICAgICAgICAgICAgICAg\nICAgIE86OG86Yi4gICAgICAgICAgICAgICAgICAgICAgICA6ODhvOiAgIGA4\nOiwgCiAgICAgICAgICAgICAgICAgICBgOE86OzdiLC4gICAgICAgICAgICAg\nICAgICAgICAgIGAiOCcgICAgIFk6IAogICAgICAgICAgICAgICAgICAgIGBZ\nTztgOGInIAogICAgICAgICAgICAgICAgICAgICBgT287IDg6LiAKICAgICAg\nICAgICAgICAgICAgICAgIGBPUCI4LmAgCiAgICAgICAgICAgICAgICAgICAg\nICAgOiAgWThQIAogICAgICAgICAgICAgICAgICAgICAgIGBvICBgLCAKICAg\nICAgICAgICAgICAgICAgICAgICAgWThib2QuIAogICAgICAgICAgICAgICAg\nICAgICAgICBgIiIiIicgCg==\n")
+end    
 
 ###############
 # main routine#
@@ -183,6 +200,8 @@ end
 
 # On load, dump the main menu
 main
+puts "-------"
+
 
 # wrapped to catch interrupt
 begin
@@ -192,6 +211,7 @@ begin
       # if there was nothing returned by the parse.. print the main menu
       main
     end
+    puts "-------"
   end
 rescue Interrupt
   # catch an interrupt (ctrl-c or 'exit')
